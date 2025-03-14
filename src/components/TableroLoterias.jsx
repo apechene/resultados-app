69e11dc6f91c00a7e18d3de27b51ababae76d06d
Ander <apechene@gmail.com>
2025-03-14 15:13:54 -0500

    ajustes en video

--- src/components/TableroLoterias.jsx
+++ src/components/TableroLoterias.jsx
@@ -89,7 +89,7 @@ const TableroLoterias = () => {
         })}
         <div className="col-12 text-center mt-4">
           <video width="" height="240" autoPlay loop muted>
-            <source src="/intro.mp4" type="video/mp4" />
+            <source src="./intro.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
