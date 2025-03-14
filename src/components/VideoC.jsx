28ab177ef523f6f3992b58830558366284b9121b
Ander <apechene@gmail.com>
2025-03-14 15:04:13 -0500

    ajustes de distro

--- src/components/VideoC.jsx
+++ src/components/VideoC.jsx
@@ -16,7 +16,7 @@ export const VideoC = () => {
         showVideo && (
             <div className="col-12 text-center mt-4">
                 <video width="" height="240" autoPlay loop muted>
-                    <source src="/intro.mp4" type="video/mp4" />
+                    <source src="./intro.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
